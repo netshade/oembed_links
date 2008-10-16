5016f9d98e736e47f1ed2ed3560abffc1267bd67
Zelenak <czelenak@INI-ID020110.local>
2008-10-16 14:16:51 -0400

    initial commit of ruby tubesday fetcher

---
+++ lib/oembed_links/fetchers/ruby_tubesday.rb
@@ -0,0 +1,23 @@
+require 'ruby_tubesday'
+
+class OEmbed
+  module Fetchers
+    class RubyTubesday
+
+      def initialize
+        @client = RubyTubesday.new(:verify_ssl => false)
+      end
+      
+      def name
+        "RubyTubesday"
+      end
+
+      def fetch(url)
+        @client.get(url)
+      end
+      
+    end
+  end
+end
+
+OEmbed.register_fetcher(OEmbed::Fetchers::RubyTubesday)
